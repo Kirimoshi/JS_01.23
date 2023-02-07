79cad87ef1b03b1f9358120c3ef4a46698428892
Ihor Kulyk <baronaxaro@gmail.com>
2023-02-07 21:25:06 +0200

    [3.2:07.02]: finished 2 out of 2 tasks

--- index.js
+++ index.js
@@ -1,128 +1,32 @@
 // Task 1
-function doCalculationsTask1() {
-  const queryUserInput = () => prompt('Please enter valid number', '5');
-  const getFactorial = (num) => {
-    let result = 1;
-    while (num !== 1) {
-      result *= num;
-      num -= 1;
-    }
-    return result;
-  };
-  const assertPrime = (num) => {
-    if (num <= 1) {
-      return false;
-    }
-    if (num % 2 === 0 && num > 2) {
-      return false;
-    }
-    const SQUARED_ROOT = Math.sqrt(num);
-    for (let i = 3; i <= SQUARED_ROOT; i += 2) {
-      if (num % i === 0) {
-        return false;
-      }
-    }
-    return true;
-  };
-  const assertEven = (num) => {
-    return num % 2 === 0;
-  };
-  const getDelimiters = (num) => {
-    let delimiters = `${num}`;
-    if (assertEven(num)) {
-      for (let i = num / 2; i > 0; i--) {
-        if (num % i === 0) {
-          delimiters += `, ${i}`;
-        }
-      }
-    } else {
-      for (let i = num - 1; i > 0; i--) {
-        if (num % i === 0) {
-          delimiters += `, ${i}`;
-        }
-      }
-    }
-    return delimiters;
-  };
-  const getResult = (validated) => {
-    return `Number: ${validated}
-Factorial: ${getFactorial(validated)}
-Square: ${Math.pow(validated, 2)}
-isPrime: ${assertPrime(validated)}
-isEven: ${assertEven(validated)}
-Delimiters: ${getDelimiters(validated)}`
-  };
-  const printCalculationsResult = (result) => {
-    console.log(result);
-  };
 
-  function validateUserInput(inputNumberType) {
-    if (Number.isNaN(inputNumberType)) {
-      console.log('Incorrect input!');
-      const inputNumberType = Number(queryUserInput());
-      return validateUserInput(inputNumberType);
-    } else if (!(Number.isFinite(inputNumberType) && Number.isInteger(inputNumberType) && inputNumberType >= 0)) {
-      console.log('Incorrect input!');
-      const inputNumberType = Number(queryUserInput());
-      return validateUserInput(inputNumberType);
-    } else {
-      return inputNumberType;
-    }
+Array.prototype.customFilter = function customFilter (callback, thisArg) {
+  let bindedCallback;
+  if (thisArg) {
+    bindedCallback = callback.bind(thisArg);
+  } else {
+    bindedCallback = callback;
   }
 
-  const inputNumberType = Number(queryUserInput());
-  const validated = validateUserInput(inputNumberType);
-  printCalculationsResult(getResult(validated));
-}
+  const filteredArr = [];
 
-doCalculationsTask1();
+  this.forEach((item, index, array) => {
+    if (bindedCallback(item, index, array)) {
+      filteredArr.push(item);
+    }
+  })
+
+  return filteredArr;
+}
 
 // Task 2
-function printMatrixTask2() {
-  const queryUserInputCharSet = () => prompt('Please enter valid character set (1..3 chars)', 'abc');
-  const queryUserInputMatrixDimension = () => prompt('Please enter valid number (1..10)', '3');
 
-  function validateUserInputCharSet(charSet) {
-    if (charSet.trim().length === 0 || charSet.trim().length > 3) {
-      console.log('Incorrect input!');
-      return validateUserInputCharSet(queryUserInputCharSet());
-    }
-    return charSet;
-  }
-
-  function validateUserInputMatrixDimension(matDim) {
-    if (Number.isNaN(matDim)) {
-      console.log('Incorrect input!');
-      return validateUserInputMatrixDimension(Number(queryUserInputMatrixDimension()));
-    } else if (!(Number.isFinite(matDim) && Number.isInteger(matDim) && matDim >= 1 && matDim <= 9)) {
-      console.log('Incorrect input!');
-      return validateUserInputMatrixDimension(queryUserInputMatrixDimension());
-    } else {
-      return matDim;
-    }
-  }
-
-  function getResult(validatedCharSet, validatedMatrixDimension) {
-    let result = `${validatedCharSet}`;
-    for (let i = 0; i < validatedMatrixDimension - 1; i++) {
-      result += ` ${validatedCharSet}`;
-    }
-    const row = result;
-    for (let i = 0; i < validatedMatrixDimension - 1; i++) {
-      result += `\n${row}`;
-    }
-    return result;
-  }
-
-  function print(result) {
-    console.log(result);
-  }
-
-  const charSet = queryUserInputCharSet();
-  const validatedCharSet = validateUserInputCharSet(charSet);
-  const matDim = Number(queryUserInputMatrixDimension());
-  const validatedMatrixDimension = validateUserInputMatrixDimension(matDim);
-  print(getResult(validatedCharSet, validatedMatrixDimension));
+function createDebounceFunction(callback, delay) {
+  let timerId;
+  return function(...args) {
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      callback.apply(this, args);
+    }, delay);
+  };
 }
-
-printMatrixTask2();
