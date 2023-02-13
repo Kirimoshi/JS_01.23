f4bde04d6938d07aa4591ca94b1fae041d20e7dd
Ihor Kulyk <baronaxaro@gmail.com>
2023-02-13 14:43:26 +0200

    [4.2:13.02]: finished 2nd task

--- index.js
+++ index.js
@@ -14,3 +14,48 @@ function curry(func) {
 
 // Task 2
 
+class Calculator {
+  constructor(x, y) {
+    if (arguments.length !== 2 || typeof x !== 'number' || typeof y !== 'number' || !Number.isFinite(x) ||
+      !Number.isFinite(y)) {
+      throw new Error();
+    }
+    this.x = x;
+    this.y = y;
+  }
+
+  static validateArg(num) {
+    if (arguments.length === 0 || typeof num !== 'number' || !Number.isFinite(num)) {
+      throw new Error();
+    }
+  }
+  static validateDivByZero(num) {
+    if (num === 0) {
+      throw new Error();
+    }
+  }
+
+  setX = (num) => {
+    Calculator.validateArg(num);
+    this.x = num;
+  }
+  setY = (num) => {
+    Calculator.validateArg(num);
+    this.y = num;
+  }
+  getSum = () => {
+    return this.x + this.y;
+  }
+  getMul = () => {
+    return this.x * this.y;
+  }
+  getSub = () => {
+    return this.x - this.y;
+  }
+  getDiv = () => {
+    Calculator.validateDivByZero(this.y);
+    return this.x / this.y;
+  }
+}
+
+
