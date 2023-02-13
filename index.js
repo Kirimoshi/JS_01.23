5938c0c1cf6174a907e32a7d2bf719fd2e41db0a
Ihor Kulyk <baronaxaro@gmail.com>
2023-02-13 16:04:38 +0200

    [4.3:13.02]: finished 3rd task

--- index.js
+++ index.js
@@ -58,4 +58,42 @@ class Calculator {
   }
 }
 
+// Task 3
 
+class RickAndMorty {
+  constructor() {}
+  static validateArg(num) {
+    if (arguments.length === 0 || typeof num !== 'number' || !Number.isFinite(num)) {
+      throw new Error();
+    }
+  }
+  getCharacter(charId) {
+    RickAndMorty.validateArg(charId);
+    return fetch(`https://rickandmortyapi.com/api/character/${charId}`)
+      .then((res) => {
+        if (res.status === 404) {
+          return null;
+        } else {
+          return res.json();
+        }
+      })
+      .catch((err) => null);
+  }
+  async getEpisode(episodeId) {
+    RickAndMorty.validateArg(episodeId);
+    try {
+      const response = await fetch(`https://rickandmortyapi.com/api/episode/${episodeId}`);
+      if (response.status === 404) {
+        return null;
+      } else {
+        return await response.json();
+      }
+    } catch (err) {
+      return null;
+    }
+  }
+}
+
+const character = new RickAndMorty();
+character.getCharacter(5).then((data) => console.log(data));
+character.getEpisode(5).then((data) => console.log(data));
