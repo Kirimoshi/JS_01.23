e70e081366e73731c7f2fc57dc394961eb7f89db
Ihor Kulyk <baronaxaro@gmail.com>
2023-02-13 21:18:55 +0200

    [5.3:13.02]: finished 3rd task

--- index.js
+++ index.js
@@ -1,95 +1,190 @@
 // Task 1
 
-function curry(func) {
-  return function curried(...args) {
-    if (args.length >= func.length) {
-      return func.apply(this, args);
+class Stack {
+  constructor(maxStackLimit?) {
+    if (maxStackLimit) {
+      if (!(Number.isFinite(maxStackLimit) && Number.isInteger(maxStackLimit) && Number.isSafeInteger(maxStackLimit)
+        && maxStackLimit > 0)) {
+        throw new Error('Invalid limit value');
+      }
+      this.maxStackLimit = maxStackLimit;
     } else {
-      return function(...args2) {
-        return curried.apply(this, args.concat(args2));
-      };
+      this.maxStackLimit = 10;
     }
-  };
+  }
 }
 
 // Task 2
 
-class Calculator {
-  constructor(x, y) {
-    if (arguments.length !== 2 || typeof x !== 'number' || typeof y !== 'number' || !Number.isFinite(x) ||
-      !Number.isFinite(y)) {
-      throw new Error();
-    }
-    this.x = x;
-    this.y = y;
-  }
+class LinkedList {
+  constructor() {}
 
-  static validateArg(num) {
-    if (arguments.length === 0 || typeof num !== 'number' || !Number.isFinite(num)) {
-      throw new Error();
-    }
-  }
-  static validateDivByZero(num) {
-    if (num === 0) {
-      throw new Error();
-    }
-  }
-
-  setX = (num) => {
-    Calculator.validateArg(num);
-    this.x = num;
-  }
-  setY = (num) => {
-    Calculator.validateArg(num);
-    this.y = num;
-  }
-  getSum = () => {
-    return this.x + this.y;
-  }
-  getMul = () => {
-    return this.x * this.y;
-  }
-  getSub = () => {
-    return this.x - this.y;
-  }
-  getDiv = () => {
-    Calculator.validateDivByZero(this.y);
-    return this.x / this.y;
-  }
 }
 
 // Task 3
 
-class RickAndMorty {
+class Car {
   constructor() {}
-  static validateArg(num) {
-    if (arguments.length === 0 || typeof num !== 'number' || !Number.isFinite(num)) {
-      throw new Error();
-    }
+
+  #brand = '';
+  #model = '';
+  #yearOfManufacturing = 1950;
+  #maxSpeed = 100;
+  #maxFuelVolume = 20;
+  #fuelConsumption = 1;
+  #damage = 1;
+  #currentFuelVolume = 0;
+  #isStarted = false;
+  #mileage = 0;
+  #health = 100;
+
+  get brand() {
+    return this.#brand;
   }
-  getCharacter = (charId) => {
-    RickAndMorty.validateArg(charId);
-    return fetch(`https://rickandmortyapi.com/api/character/${charId}`)
-      .then((res) => {
-        if (res.status === 404) {
-          return null;
-        } else {
-          return res.json();
-        }
-      })
-      .catch((err) => null);
+  get model() {
+    return this.#model;
   }
-  getEpisode = async (episodeId) => {
-    RickAndMorty.validateArg(episodeId);
-    try {
-      const response = await fetch(`https://rickandmortyapi.com/api/episode/${episodeId}`);
-      if (response.status === 404) {
-        return null;
-      } else {
-        return await response.json();
-      }
-    } catch (err) {
-      return null;
+  get yearOfManufacturing() {
+    return this.#yearOfManufacturing;
+  }
+  get maxSpeed() {
+    return this.#maxSpeed;
+  }
+  get maxFuelVolume() {
+    return this.#maxFuelVolume;
+  }
+  get fuelConsumption() {
+    return this.#fuelConsumption;
+  }
+  get damage() {
+    return this.#damage;
+  }
+  get currentFuelVolume() {
+    return this.#currentFuelVolume;
+  }
+  get isStarted() {
+    return this.#isStarted;
+  }
+  get mileage() {
+    return this.#mileage;
+  }
+  get health() {
+    return this.#health;
+  }
+
+
+  set brand(brandName) {
+    if (!(typeof brandName === 'string' && brandName.length >= 1 && brandName.length <= 50)) {
+      throw new Error('Invalid brand name');
     }
+    this.#brand = brandName;
+  }
+  set model(modelName) {
+    if (!(typeof modelName === 'string' && modelName.length >= 1 && modelName.length <= 50)) {
+      throw new Error('Invalid model name');
+    }
+    this.#model = modelName;
+  }
+  set yearOfManufacturing(year) {
+    if (!(Number.isFinite(year) && year >= 1950 && year <= 2023)) {
+      throw new Error('Invalid year of manufacturing');
+    }
+    this.#yearOfManufacturing = year;
+  }
+  set maxSpeed(maxSpeed) {
+    if (!(Number.isFinite(maxSpeed) && maxSpeed >= 100 && maxSpeed <= 330)) {
+      throw new Error('Invalid max speed');
+    }
+    this.#maxSpeed = maxSpeed;
+  }
+  set maxFuelVolume(maxFuelVolume) {
+    if (!(Number.isFinite(maxFuelVolume) && maxFuelVolume >= 20 && maxFuelVolume <= 100)) {
+      throw new Error('Invalid max fuel volume');
+    }
+    this.#maxFuelVolume = maxFuelVolume;
+  }
+  set fuelConsumption(fuelConsumption) {
+    if (!(Number.isFinite(fuelConsumption) && fuelConsumption > 0)) {
+      throw new Error('Invalid fuel consumption');
+    }
+    this.#fuelConsumption = fuelConsumption;
+  }
+  set damage(damage) {
+    if (!(Number.isFinite(damage) && damage >= 1 && damage <= 5)) {
+      throw new Error('Invalid damage');
+    }
+    this.#damage = damage;
+  }
+
+  start() {
+    if (this.#isStarted) {
+      throw new Error('Car has already started');
+    }
+    this.#isStarted = true;
+  }
+  shutDownEngine() {
+    if (!this.#isStarted) {
+      throw new Error(`Car hasn't started yet`);
+    }
+    this.#isStarted = false;
+  }
+  fillUpGasTank(fuelAmount) {
+    if (!(Number.isFinite(fuelAmount) && fuelAmount > 0)) {
+      throw new Error('Invalid fuel amount');
+    }
+    if (this.#isStarted) {
+      throw new Error('You have to shut down your car first');
+    }
+    if (this.#currentFuelVolume + fuelAmount > this.#maxFuelVolume) {
+      throw new Error('Too much fuel');
+    }
+    this.#currentFuelVolume = this.#currentFuelVolume + fuelAmount;
+  }
+  drive(speed, hours) {
+    if (!(Number.isFinite(speed) && speed > 0)) {
+      throw new Error('Invalid speed');
+    }
+    if (!(Number.isFinite(hours) && hours > 0)) {
+      throw new Error('Invalid duration');
+    }
+    if (speed > this.#maxSpeed) {
+      throw new Error(`Car can't go this fast`);
+    }
+    if (!this.#isStarted) {
+      throw new Error(`You have to start your car first`);
+    }
+    const estimateEndFuelAmount = () => {
+      return this.#currentFuelVolume - (this.#fuelConsumption / 100 * speed * hours);
+    }
+    if (this.#currentFuelVolume === 0 || estimateEndFuelAmount() < 0) {
+      throw new Error(`You don't have enough fuel`);
+    }
+    const estimateEndHealthValue = () => {
+      return this.#health - (this.#damage / 100 * speed * hours);
+    }
+    if (this.#health === 0 || estimateEndHealthValue() <= 0) {
+      throw new Error(`Your car won’t make it`);
+    }
+    const estimateMileage = () => {
+      return this.#mileage + speed * hours;
+    }
+    this.#currentFuelVolume = estimateEndFuelAmount();
+    this.#health = estimateEndHealthValue();
+    this.#mileage = estimateMileage();
+  }
+  repair() {
+    if (this.#isStarted) {
+      throw new Error('You have to shut down your car first');
+    }
+    if (this.#currentFuelVolume === this.#maxFuelVolume) {
+      throw new Error(`You have to fill up your gas tank first`);
+    }
+    this.#health = 100;
+  }
+  getFullAmount() {
+    if (this.#currentFuelVolume === this.#maxFuelVolume) {
+      return 0;
+    }
+    return this.#maxFuelVolume - this.#currentFuelVolume;
   }
 }
